db4074e83ebea94e4b09b13d0c775f8906209423
je-lopez <je.lopez@me.com>
2018-01-03 16:04:27 -0800

    adds class to wrapper for styles

---
+++ src/components/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import Nav from './Nav';
+import Home from './Home';
+import Battle from './Battle';
+import Results from './Results';
+import Popular from './Popular';
+
+import './index.css';
+
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div className='container'>
+          <Nav />
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route exact path='/battle' component={Battle} />
+            <Route path='/battle/results' component={Results} />
+            <Route path='/popular' component={Popular} />
+            <Route render={() =>
+              <p>Not Found</p>
+            } />
+          </Switch>
+        </div>
+      </BrowserRouter>
+    );
+  }
+}
+
+export default App;
